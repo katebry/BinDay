f07845b16ca153b0f242b4281df5a69c44f196d4
k9bry <46564417+katebry@users.noreply.github.com>
2021-09-07 20:19:04 +0100

    feat: added btn and onClick listener

--- BinDay/app/src/main/java/com/example/binday/MainActivity.kt
+++ BinDay/app/src/main/java/com/example/binday/MainActivity.kt
@@ -2,10 +2,19 @@ package com.example.binday
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val whichBinBtn = findViewById<Button>(R.id.btn_whichBin)
+
+        whichBinBtn.setOnClickListener {
+            // your code to perform when the user clicks on the button
+            Toast.makeText(this@MainActivity, "Put out the x and y bins.", Toast.LENGTH_LONG).show()
+        }
     }
 }
